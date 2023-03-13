0a6e5e6a2d53960946383dfee06a57713153e37b
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-03-13 10:36:22 -0400

    fixed hero section with styling and content, also included profile picture with NextJS image optimization for smoother loading purposes across all platforms

--- jen-portfolio/components/BackgroundCircles.tsx
+++ jen-portfolio/components/BackgroundCircles.tsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {motion} from 'framer-motion';
 
-type Props = {}
+type Props = {};
 
 const BackgroundCircles = (props: Props) => {
   return (
-    <div className='relative flex justify-center items-center'>
+    <motion.div 
+      initial={{opacity: 0}}
+      animate={{
+        scale: [1, 2, 2, 3, 1],
+        opacity: [0.1, 0.2, 0.4, 0.8, 0.1, 1.0],
+        borderRadius: ['20%', '20%', '50%', '80%', '20%']
+      }}
+      transition={{duration: 2.5}}
+      className='relative flex justify-center items-center'
+    >
       <div className='absolute border border-[#333333] rounded-full h-[200px] w-[200px] mt-52 animate-ping'/>
-      <div className='absolute border border-[#333333] rounded-full h-[300px] w-[300px] mt-52 animate-ping'/>
-      <div className='absolute border border-[#333333] rounded-full h-[400px] w-[400px] mt-52 animate-ping'/>
-      <div className='absolute border border-[#333333] rounded-full h-[650px] w-[650px] opacity-20 mt-52 animate-ping'/>
-      <div className='absolute border border-[#333333] rounded-full h-[800px] w-[800px] opacity-20 mt-52 animate-ping'/>
-    </div>
+      <div className='rounded-full border border-[#333333] h-[300px] w-[300px] absolute mt-52'/>
+      <div className='rounded-full border border-[#333333] h-[500px] w-[500px] absolute mt-52'/>
+      <div className='absolute border border-[#FBC101] rounded-full h-[650px] w-[650px] opacity-20 mt-52 animate-pulse'/>
+      <div className='absolute border border-[#333333] rounded-full h-[800px] w-[800px] mt-52'/>
+    </motion.div>
   )
-}
+};
 
-export default BackgroundCircles
+export default BackgroundCircles;
--- jen-portfolio/components/Hero.tsx
+++ jen-portfolio/components/Hero.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'next/image';
+import profilePic from './../public/image0.jpeg';
 import {useTypewriter} from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
 
@@ -13,9 +15,19 @@ const Hero = (props: Props) => {
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overlow-hidden'>
       <BackgroundCircles />
-      <h1>Hello, my name is Jen Tchai</h1>
-      <h2>I am a Frontend Developer, based in Washington, DC.</h2>
-      <div>{text}</div>
+      <Image
+        src={profilePic}
+        height={200}
+        width={200}
+        className='roundedImg'
+        placeholder='blur'
+        alt='profile image of the developer' 
+      />
+      <div>
+        <h1>Hello, my name is Jen Tchai</h1>
+        <h2>I am a Frontend Developer, based in Washington, DC.</h2>
+        <div>{text}</div>
+      </div>
     </div>
   )
 }
