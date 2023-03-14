d9dc8fad8ccf4adf0d6150fd06fcbaa426255dda
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-03-14 14:01:43 -0400

    set up Skills section

--- jen-portfolio/components/Skill.tsx
+++ jen-portfolio/components/Skill.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {motion} from 'framer-motion';
 
-type Props = {}
+type Props = {
+  directionLeft?: boolean;
+};
 
-const Skill = (props: Props) => {
+const Skill = ({directionLeft}: Props) => {
   return (
-    <div>testingggg</div>
+    <div className='group relative flex cursor-pointer'>
+      <motion.img 
+          initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0
+          }}
+          whileInView={{
+            x: 0,
+            opacity: 1
+          }}
+          transition={{duration: 1.5}}
+          src='./../public/image0.jpeg'
+          className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition uration-300 ease-in-out'
+        />
+        <div className='absolute opacity-100 h-24 s-24 m:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+          <div className='flex items-center justify-center h-full'>
+            <p className='text-3xl font-bold'>hello</p>
+          </div>
+        </div>
+    </div>
   )
 }
 
--- jen-portfolio/components/Skills.tsx
+++ jen-portfolio/components/Skills.tsx
@@ -7,10 +7,25 @@ type Props = {};
 const Skills = (props: Props) => {
   return (
     <>
-      <motion.div className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'>
+      <motion.div 
+        initial={{
+          x: -200,
+          opacity: 0
+        }}
+        whileInView={{x: 0, opacity: 1}}
+        transition={{duration: 1.5}}
+        className='h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center'
+      >
         <h3 className='absolute top-24 uppercase tracking-[5px] text-gray-500 text-2xl'>Skills</h3>
         <h3></h3>
         <div className='grid grid-cols-4 gap-5'>
+          <Skill />
+          <Skill />
+          <Skill />
+          <Skill />
+          <Skill />
+          <Skill />
+          <Skill />
           <Skill />
           <Skill />
           <Skill />
