702a3af40b1d7dde275768ebdb614a19eca686f3
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-03-16 15:53:12 -0400

    fixed mobile version of Projects section with tweaking Tailwind code

--- jen-portfolio/components/ContactMe.tsx
+++ jen-portfolio/components/ContactMe.tsx
@@ -14,81 +14,76 @@ type Props = {};
 
 const ContactMe = (props: Props) => {
   const {register, handleSubmit} = useForm<Inputs>();
-  // const onSubmit: SubmitHandler<Inputs> = (formData) => window.location.href = `mailto:jentchai@gmail.com?subject=${formData.subject}&body=My name is ${formData.name} and my email is ${formData.email}. ${formData.message}`;
-
   return (
     <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[5px] text-gray-500 text-2xl'>
         Contact Me
-      </h3>
-      <div className='flex flex-col space-y-10'>
-        <h4 className='text-4xl font-semibold text-center'>This is where you can find me.<br/>
+      </h3><br/>
+      <div className='flex flex-col space-y-5'>
+        <h4 className='text-lg md:text-4xl font-semibold text-center'>This is where you can find me.<br/>
           <span className='decoration-[#FBC101]/50 underline'>Let's connect.</span>
         </h4>
-        <div className='space-y-5'>
-          <div className='flex items-left space-x-5 justify-left ml-20'>
-              <SocialIcon
-                url='https://www.github.com/hopesunknown' 
-                target='_blank'
-                fgColor='transparent' 
-                bgColor='#FBC101'
-                className='hoverSocialBtn' 
-              />
-              <a href='https://www.github.com/hopesunknown' target='_blank' className='socialUrlLink'>
-                github.com/hopesunknown
-              </a>
-            </div>
-
-            <div className='flex items-left space-x-5 justify-left ml-20'>
-              <SocialIcon 
-                url='https://www.linkedin.com/in/jentchai/' 
-                target='_blank'
-                fgColor='transparent' 
-                bgColor='#FBC101'
-                className='hoverSocialBtn'  
-              />
-              <a href='https://www.linkedin.com/in/jentchai/' target='_blank' className='socialUrlLink'>
-                linkedin.com/in/jentchai
-              </a>
-            </div>
-
-            <div className='flex items-left space-x-5 justify-left ml-20'>
-              <SocialIcon
-                url='https://www.youtube.com/@jentchai1728' 
-                target='_blank'
-                fgColor='transparent' 
-                bgColor='#FBC101'
-                className='hoverSocialBtn' 
-              />
-              <a href='https://www.youtube.com/@jentchai1728' target='_blank' className='socialUrlLink'>
-                youtube.com/@jentchai1728
-              </a>
-            </div>
-            <div className='flex items-left space-x-5 justify-left ml-20'>
-              <SocialIcon 
-                url='https://medium.com/@jentchai' 
-                target='_blank'
-                fgColor='transparent' 
-                bgColor='#FBC101'
-                className='hoverSocialBtn' 
-              />
-              <a href='https://medium.com/@jentchai' target='_blank' className='socialUrlLink'>
-                medium.com/@jentchai
-              </a>
-            </div>
+        <div className='space-y-2 md:space-y-5'>
+          <div className='flex items-left space-x-5 justify-left ml-5 md:ml-0'>
+            <SocialIcon
+              url='https://www.github.com/hopesunknown' 
+              target='_blank'
+              fgColor='transparent' 
+              bgColor='#FBC101'
+              className='hoverSocialBtn' 
+            />
+            <a href='https://www.github.com/hopesunknown' target='_blank' className='socialUrlLink'>
+              github.com/hopesunknown
+            </a>
           </div>
-
+          <div className='flex items-left space-x-5 justify-left ml-5 md:ml-0'>
+            <SocialIcon 
+              url='https://www.linkedin.com/in/jentchai/' 
+              target='_blank'
+              fgColor='transparent' 
+              bgColor='#FBC101'
+              className='hoverSocialBtn'  
+            />
+            <a href='https://www.linkedin.com/in/jentchai/' target='_blank' className='socialUrlLink'>
+              linkedin.com/in/jentchai
+            </a>
+          </div>
+          <div className='flex items-left space-x-5 justify-left ml-5 md:ml-0'>
+            <SocialIcon
+              url='https://www.youtube.com/@jentchai1728' 
+              target='_blank'
+              fgColor='transparent' 
+              bgColor='#FBC101'
+              className='hoverSocialBtn' 
+            />
+            <a href='https://www.youtube.com/@jentchai1728' target='_blank' className='socialUrlLink'>
+              youtube.com/@jentchai1728
+            </a>
+          </div>
+          <div className='flex items-left space-x-5 justify-left ml-5 md:ml-0'>
+            <SocialIcon 
+              url='https://medium.com/@jentchai' 
+              target='_blank'
+              fgColor='transparent' 
+              bgColor='#FBC101'
+              className='hoverSocialBtn' 
+            />
+            <a href='https://medium.com/@jentchai' target='_blank' className='socialUrlLink'>
+              medium.com/@jentchai
+            </a>
+          </div>
+        </div>
         <form 
           // onSubmit={handleSubmit(onSubmit)} 
           action='https://formsubmit.co/e725e159ba0dc850132dc2683d0df0f9'
           method='POST'
           target='_blank'
-          className='flex flex-col space-y-2 w:screen md:w-fit mx-auto'
+          className='flex flex-col space-y-1 md:space-y-2 w:screen w-[200px] md:w-fit mx-auto'
         >
-          <div className='flex space-x-2'>
+          {/* <div className='flex space-x-5 md:my-0 w-fit md:space-x-2'> */}
             <input {...register('name')} className='contactInput' placeholder='Name' type='text' />
+          {/* </div> */}
             <input {...register('email')} className='contactInput' placeholder='Email' type='email' />
-          </div>
           <input {...register('subject')} className='contactInput' placeholder='Subject' type='text' />
           <textarea {...register('message')} className='contactInput' placeholder='Message' />
           <button className='heroButton'>
--- jen-portfolio/components/Projects.tsx
+++ jen-portfolio/components/Projects.tsx
@@ -19,12 +19,12 @@ const Projects = ({projects}: Props) => {
       transition={{duration: 1.5}}
       className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'
     >
-      <h3 className='absolute top-24 uppercase tracking-[5px] text-gray-500 text-2xl'>
+      <h3 className='absolute top-24 uppercase tracking-[5px] text-gray-500 text-xl md:text-2xl'>
         Projects
       </h3>
       <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-track-gray-500/10 scrollbar-thumb-[#FBC101]/80'>
         {projects?.map((project, index) => (
-          <div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+          <div key={index} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-4 items-center justify-center p-20 md:p-44 h-screen'>
             {/* <motion.img 
               initial={{
                 x: -300,
@@ -44,18 +44,18 @@ const Projects = ({projects}: Props) => {
               width={350}
               height={200}
             />
-          <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
-            <h4 className='text-4xl font-semibold text-center'>
+          <div className='space-y-6 px-0 md:px-10 max-w-6xl'>
+            <h4 className='text-md md:text-4xl font-semibold text-center'>
               <span className='underline decoration-[#FBC101]/50'>Project {index + 1} of {projects.length}</span><br/>
                 {project?.title}
-              </h4><br/>
+              </h4>
               <div className='flex items-center space-x-2 justify-center'>
                 {project?.technologies.map((technology) => (
                   <img 
                     key={technology._id} 
                     src={urlFor(technology.image).url()} 
                     alt='technology icon' 
-                    className='h-10 w-10'
+                    className='h-5 w-5 md:h-10 md:w-10'
                   />
                 ))}
               </div><br/>
@@ -68,11 +68,10 @@ const Projects = ({projects}: Props) => {
                   target='_blank'
                   fgColor='transparent' 
                   bgColor='#FBC101'
-                  style={{margin: 5}}
+                  style={{marginLeft: 5, marginRight: 5}}
                   className='hoverSocialBtn' 
                 />
               </center>
-              
             </div>
           </div>
         ))}
