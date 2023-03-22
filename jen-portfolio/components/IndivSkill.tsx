272204157de7cf024016548aed7233cf0049846d
Jen Tchai <69021958+hopesunknown@users.noreply.github.com>
2023-03-22 10:43:38 -0400

    made skill icons less opacity for better visibility and contrast with the skill names displayed. Also made skill icons and bubbles smaller to fit on mobile better, especially on Safari

--- jen-portfolio/components/IndivSkill.tsx
+++ jen-portfolio/components/IndivSkill.tsx
@@ -22,9 +22,9 @@ const IndivSkill = ({skill, directionLeft}: Props) => {
           }}
           transition={{duration: 1.2}}
           src={urlFor(skill?.image).url()}
-          className='rounded-full border border-gray-500 object-cover w-20 h-20 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out items-center'
+          className='rounded-full border border-gray-500 object-cover w-16 h-16 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out items-center'
         />
-        <div className='absolute opacity-100 h-20 w-20 m:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
+        <div className='absolute opacity-100 h-16 w-16 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-50'>
           <div className='flex items-center justify-center h-full'>
             <p className='text-sm md:text-xl font-semibold'>{skill?.title}</p>
           </div>
